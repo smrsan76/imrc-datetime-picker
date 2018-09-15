6d3feecaa301dc6c2bfa5f484bae69bd1f85b511
smrsan76 <smrsan76@users.noreply.github.com>
2018-09-16 01:39:53 +0430

    Empty 'gh-pages' branch

--- src/index.js
+++
@@ -1,4 +0,0 @@
-export {default as DatetimePicker} from './Picker.jsx';
-export {default as DatetimeRangePicker} from './Range.jsx';
-export {default as DatetimePickerTrigger} from './Trigger.jsx';
-export {default as DatetimeRangePickerTrigger} from './RangeTrigger.jsx';
--- src/sass/index.js
+++
@@ -1,1 +0,0 @@
-module.exports = require("./imrc-datetime-picker.scss");
