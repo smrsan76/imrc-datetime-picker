1089d58108c6c9c0426b9cb1154e00cae71ec86d
smrsan76 <smrsan76@users.noreply.github.com>
2018-10-02 00:46:26 +0330

    Persian lang and numbers consts :: Added - & - Solar consts :: Added

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,88 @@
-export const WEEKS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-export const MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-export const DAY_FORMAT = 'MMMM, YYYY';
-export const CONFIRM_BUTTON_TEXT = 'Confirm';
-export const START_DATE_TEXT = 'Start Date:';
-export const END_DATE_TEXT = 'End Date:';
-export const CUSTOM_BUTTON_TEXT = 'Custom';
+export const WEEKS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+export const WEEKS_FA = ["ش", "ی", "د", "س", "چ", "پ", "ج"];
+
+export const MONTHS = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+export const MONTHS_FA = [
+  "ژانویه",
+  "فوریه",
+  "مارس",
+  "آوریل",
+  "مه",
+  "ژوئن",
+  "ژوئیه",
+  "اوت",
+  "سپتامبر",
+  "اکتبر",
+  "نوامبر",
+  "دسامبر"
+];
+
+export const MONTHS_SOLAR = [
+  "Farvardin",
+  "Ordibehesht",
+  "Khordaad",
+  "Tir",
+  "Amordaad",
+  "Shahrivar",
+  "Mehr",
+  "Aabaan",
+  "Aazar",
+  "Dey",
+  "Bahman",
+  "Esfand"
+];
+export const MONTHS_SOLAR_FA = [
+  "فروردین",
+  "اردیبهشت",
+  "خرداد",
+  "تیر",
+  "مرداد",
+  "شهریور",
+  "مهر",
+  "آبان",
+  "آذر",
+  "دی",
+  "بهمن",
+  "اسفند"
+];
+
+export const DAY_FORMAT = "MMMM, YYYY";
+export const DAY_FORMAT_SOLAR = "jMMMM, jYYYY";
+
+export const CONFIRM_BUTTON_TEXT = "Confirm";
+export const CONFIRM_BUTTON_TEXT_FA = "تایید";
+
+export const START_DATE_TEXT = "Start Date:";
+export const START_DATE_TEXT_FA = "تاریخ شروع:";
+
+export const END_DATE_TEXT = "End Date:";
+export const END_DATE_TEXT_FA = "تاریخ پایان:";
+
+export const CUSTOM_BUTTON_TEXT = "Custom";
+export const CUSTOM_BUTTON_TEXT_FA = "انتخابی";
+
+export const PERSIANN_NUMBERS = [
+  "۰",
+  "۱",
+  "۲",
+  "۳",
+  "۴",
+  "۵",
+  "۶",
+  "۷",
+  "۸",
+  "۹"
+];
